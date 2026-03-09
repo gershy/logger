5fa9da8065f9625afaf4950a07f051057e733899
Gershom Maes <gershommaes@protonmail.com>
2026-03-08 21:42:40 -0400

    update deps

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,4 +1,4 @@
-import { assertEqual } from '../build/utils.test.ts';
+import { assertEqual, testRunner } from '../build/utils.test.ts';
 import './main.ts';
 
 // Type testing
@@ -12,36 +12,15 @@ import './main.ts';
   
 })();
 
-// Test cases
-(async () => {
+testRunner([
   
-  const cases = [
-    
-    {
-      name: 'not implemented',
-      fn: async () => {
-        
-        // TODO: Implement!
-        
-      }
-    }
-    
-  ];
-  for (const { name, fn } of cases) {
-    
-    try {
+  {
+    name: 'not implemented',
+    fn: async () => {
       
-      await fn();
-      
-    } catch (err: any) {
-      
-      console.log(`FAILED: "${name}"`, err[limn]());
-      process.exit(1);
+      // TODO: Implement!
       
     }
-    
   }
   
-  console.log(`Passed ${cases.length} test${cases.length === 1 ? '' : 's'}`);
-  
-})();
+]);
